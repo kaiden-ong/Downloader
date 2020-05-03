509c34d7e09f4ed2b5889351fb7cf33a375608ca
Kaiden <kaiden.ong000@gmail.com>
2020-05-02 22:08:42 -0700

    Download PDF

---
+++ app.py
@@ -0,0 +1,19 @@
+import wget
+from bs4 import BeautifulSoup
+
+wget.download("http://www.wallace.ccfaculty.org/book/book.html", "book.html")
+with open("book.html", "r") as file:
+    data = file.read() 
+print(data)
+
+soup = BeautifulSoup(data, 'html.parser')
+for link in soup.find_all('a'):
+    print(link.get('href'))
+    href = link.get('href')
+    
+    # does href start with "http://"?
+    if (href.startswith("http://")):
+        continue
+
+    url = "http://www.wallace.ccfaculty.org/book/" + href
+    wget.download(url, href)
